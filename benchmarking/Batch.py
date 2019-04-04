0738d3939c07fece61e843bea5193af1e452aea2
Chris Tralie <chris.tralie@gmail.com>
2019-04-04 18:48:22 -0400

    Adding a helper script to run multiple batches

---
+++ benchmarking/Batch.py
@@ -0,0 +1,13 @@
+import subprocess
+from sys import argv
+import time
+
+# 11175 batches
+start = int(argv[1])
+end = int(argv[2])
+tic = time.time()
+for i in range(start, end):
+    subprocess.call(["python", "ChenFusion.py", "-s", "benchmarking", "-d", "../features_benchmark", "-i", "%i"%i])
+    t = time.time()-tic
+    print("Total Elapsed Time: %.3g"%(t))
+    print("Avg time per iteration: %.3g"%(t/(i-start+1)))
