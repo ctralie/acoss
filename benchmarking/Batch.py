06639bad35a004d88774256f6c80ef332ae1114a
Chris Tralie <chris.tralie@gmail.com>
2019-04-08 09:40:02 -0400

    Switching to CREMA now

--- benchmarking/Batch.py
+++ benchmarking/Batch.py
@@ -7,7 +7,7 @@ start = int(argv[1])
 end = int(argv[2])
 tic = time.time()
 for i in range(start, end):
-    subprocess.call(["python", "ChenFusion.py", "-s", "benchmarking", "-d", "../features_benchmark", "-i", "%i"%i])
+    subprocess.call(["python", "ChenFusion.py", "-c", "crema", "-s", "crema_benchmarking", "-d", "../crema_benchmark", "-i", "%i"%i])
     t = time.time()-tic
     print("Total Elapsed Time: %.3g"%(t))
     print("Avg time per iteration: %.3g"%(t/(i-start+1)))
